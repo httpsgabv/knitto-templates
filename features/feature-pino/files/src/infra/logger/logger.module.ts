da2b6769e81e37b61004f15830ca5b6d3638b99b
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2026-04-27 19:20:04 -0300

    commit

---
+++ features/feature-pino/files/src/infra/logger/logger.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
+
+@Module({
+  imports: [
+    PinoLoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.LOG_LEVEL ?? 'info',
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? {
+                target: 'pino-pretty',
+                options: {
+                  singleLine: true,
+                },
+              }
+            : undefined,
+        redact: {
+          paths: [
+            'req.headers.authorization',
+            'req.headers.cookie',
+            'req.body.password',
+            'req.body.accessToken',
+            'req.body.refreshToken',
+          ],
+          remove: true,
+        },
+      },
+    }),
+  ],
+})
+export class LoggerModule {}
