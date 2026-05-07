0d6f1d8b2324bb7cf2cdc5a129216a2ada020a20
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2026-05-06 23:59:55 -0300

    throttler

---
+++ features/throttler-nest/src/app-throttler.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { minutes, seconds, ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+
+@Module({
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          name: "short",
+          ttl: seconds(1),
+          limit: 10
+        },
+        {
+          name: "default",
+          ttl: minutes(1),
+          limit: 100
+        }
+      ]
+    })
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ]
+})
+export class AppThrottlerModule {}
