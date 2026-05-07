6ff11f0e1f9e21c2ccdbc16e529dbfad26817e58
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2026-05-06 23:45:49 -0300

    helmet

---
+++ features/helmet-nest/src/helmet.ts
@@ -0,0 +1,26 @@
+import { INestApplication } from '@nestjs/common';
+import { EnvService } from '@infra/env/env.service';
+import helmet from 'helmet';
+
+export class Helmet {
+  constructor(
+    private readonly app: INestApplication,
+    private readonly env: EnvService
+  ) {}
+
+  setup() {
+    const isProduction = this.env.isProduction();
+    this.app.use(helmet({
+      contentSecurityPolicy: isProduction ? {
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'", "'unsafe-inline'", 'https:'],
+          styleSrc: ["'self'", "'unsafe-inline'", 'https:'],
+          imgSrc: ["'self'", 'data:', 'https:'],
+          fontSrc: ["'self'", 'data:', 'https:'],
+          connectSrc: ["'self'", 'https:']
+        }
+      } : false
+    }))
+  }
+}
