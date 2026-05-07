90e413a02c393b52f4cc5fba7f984bab55423ca6
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2026-05-06 23:08:56 -0300

    scalar

---
+++ features/scalar-nest/src/scalar.ts
@@ -0,0 +1,35 @@
+import { EnvService } from '@infra/env/env.service';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { INestApplication } from '@nestjs/common';
+import { apiReference } from '@scalar/nestjs-api-reference';
+
+export class Scalar {
+  constructor(
+    private readonly app: INestApplication,
+    private readonly env: EnvService,
+  ) {}
+
+  setup() {
+    const openApiConfig = new DocumentBuilder()
+      .setTitle(this.env.get('OPENAPI_APP_NAME'))
+      .setDescription(this.env.get('OPENAPI_APP_DESCRIPTION'))
+      .setVersion(this.env.get('APP_VERSION'))
+      .addBearerAuth()
+      .build();
+    
+    const openApiDocument = SwaggerModule.createDocument(this.app, openApiConfig);
+
+    SwaggerModule.setup('openapi', this.app, openApiDocument, {
+      ui: false,
+      jsonDocumentUrl: 'openapi.json',
+    });
+
+    this.app.use(
+      '/reference',
+      apiReference({
+        content: openApiDocument,
+        theme: 'purple'
+      })
+    )
+  }
+}
